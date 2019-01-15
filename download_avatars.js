a9e6de2cb6ea491a6a8af75b3efdb45dea389583
Erin Toth <erintoth@gmail.com>
2019-01-15 02:26:43 +0000

    Program functional as per Step 8 parameters

--- download_avatars.js
+++ download_avatars.js
@@ -30,23 +30,27 @@ function downloadImageByURL(url, filePath) {
     throw err;
   })
     .on('response', function (response) {
-    console.log('Downloading...')
+    // console.log('Downloading...')
   })
   .on('end', function (response) {
+    console.log('Downloading...');
+  })
+  .pipe(fs.createWriteStream(filePath)
+    )
+  .on('finish', function (response) {
     console.log('Download complete!');
   })
-  .pipe(fs.createWriteStream(filePath));
+
 };
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
-
 
 getRepoContributors("jquery", "jquery", function(err, collaborators) {
   if (err) {
     return console.log("Errors:", err);
   }
   collaborators.forEach(function(collaborator) {
-    console.log(collaborator.avatar_url);
+    var url = collaborator.avatar_url;
+    var filePath = "avatars/" + collaborator.login + ".jpg";
+    downloadImageByURL(url, filePath);
+
   });
-  // console.log("Result:", result);
 });
