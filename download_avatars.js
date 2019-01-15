f2a4200efc9f5110a4ec0232bffad0f0861497af
Erin Toth <erintoth@gmail.com>
2019-01-15 01:08:36 +0000

    added github auth token functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,24 @@
 var request = require('request');
+require('dotenv').config();
+
+const access_token = process.env.GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    qs: {
+      access_token: process.env.GITHUB_TOKEN
+    },
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
